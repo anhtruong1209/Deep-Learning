6a96a547aea3a58273cb3a0e7c225618cd502a8c
Nguyễn Đinh Anh Trường <60927449+anhtruong1209@users.noreply.github.com>
2021-06-15 17:35:49 +0700

    Add files via upload
    Change download = True in DataLoader for download MNISTdataset

---
+++ MNIST_pytorch/utils.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt 
+import numpy as np 
+import torch 
+
+
+def multiclass_accuracy(y_pred,y_true):
+    top_p,top_class = y_pred.topk(1,dim = 1)
+    equals = top_class == y_true.view(*top_class.shape)
+    return torch.mean(equals.type(torch.FloatTensor))
+
+def view_classify(img, ps):
+
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze(),cmap = 'gray')
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels(np.arange(10))
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+
+    plt.tight_layout()
+
+    return None
